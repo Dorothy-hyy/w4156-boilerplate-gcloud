0598ee071bfa52c690721cc1a838aa77cf515364
chenKuer <chenfjk@gmail.com>
2018-02-28 21:03:22 -0500

    add uesr control

--- user/user_function.py
+++ user/user_function.py
@@ -1,1 +1,25 @@
 import unittest
+
+class User:
+    # This is user infor, the users are stored in a dictionary
+    users ={}
+
+    def create_user(userName):
+        if userName == None or userName == "":
+            return "Invalid userName"
+        if userName not in users: # The userName is unique
+            users[userName] = 0 #Initializa as a number 0, we can set it as a list to store password and other information.
+            print(userName,"create successfully")
+            return users
+        else:
+            return "Username already exist, please try another one."
+
+    def delete_user(userName):
+        if userName==None or userName =="":
+            return "Invalid userName"
+        if userName in users: # The userName is unique
+            del users[userName] #Initializa as a number 0, we can set it as a list to store password and other information.
+            print(userName,"delete successfully")
+            return users
+        else:
+            return "Username doesn't exist, please try another one."
